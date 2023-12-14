d0942663c9e23a3ed401b46a3d6275b151073619
Lalit Bharindwal <lalitbharindwal@gmail.com>
2023-12-15 00:55:12 +0530

    Update

--- techmark-gmail-api.js
+++ techmark-gmail-api.js
@@ -3,24 +3,26 @@ function extractCodeFromUrl() {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get('code');
   }
-  
 const authorizationCode = extractCodeFromUrl();
-  if (authorizationCode) {
-      console.log(code)
-      localStorage.setItem("access-token", authorizationCode)
-  }
+
 
 // Function to initiate OAuth flow
 function startOAuthFlow(clientId, redirect_uri) {
+    authorizationCode = extractCodeFromUrl();
     console.log(localStorage.getItem("access-token"))
-    if(localStorage.getItem("access-token") == null){
-        const authorizationEndpoint = 'https://accounts.google.com/o/oauth2/auth';
-        const responseType = 'code';
+    if (authorizationCode) {
+        console.log(code)
+        localStorage.setItem("access-token", authorizationCode)
+    }else{
+        if(localStorage.getItem("access-token") == null){
+            const authorizationEndpoint = 'https://accounts.google.com/o/oauth2/auth';
+            const responseType = 'code';
 
-        // Construct the authorization URL
-        const authUrl = `${authorizationEndpoint}?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${responseType}&scope=${scope}`;
+            // Construct the authorization URL
+            const authUrl = `${authorizationEndpoint}?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${responseType}&scope=${scope}`;
 
-        // Redirect user to the authorization URL
-        window.location.href = authUrl;
+            // Redirect user to the authorization URL
+            window.location.href = authUrl;
+        }
     }
 }
