474de40d6cbdca6c80c74b98a7721552eca691f4
Lalit Bharindwal <lalitbharindwal@gmail.com>
2023-12-15 00:40:18 +0530

    update

--- techmark-gmail-api.js
+++ techmark-gmail-api.js
@@ -11,13 +11,14 @@ const authorizationCode = extractCodeFromUrl();
 
 // Function to initiate OAuth flow
 function startOAuthFlow(clientId, redirect_uri) {
-  const authorizationEndpoint = 'https://accounts.google.com/o/oauth2/auth';
-  const responseType = 'code';
+    if(localStorage.getItem("access-token") == null){
+        const authorizationEndpoint = 'https://accounts.google.com/o/oauth2/auth';
+        const responseType = 'code';
 
-  // Construct the authorization URL
-  const authUrl = `${authorizationEndpoint}?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${responseType}&scope=${scope}`;
+        // Construct the authorization URL
+        const authUrl = `${authorizationEndpoint}?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${responseType}&scope=${scope}`;
 
-  // Redirect user to the authorization URL
-  window.location.href = authUrl;
-  
+        // Redirect user to the authorization URL
+        window.location.href = authUrl;
+    }
 }
