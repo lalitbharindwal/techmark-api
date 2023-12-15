850d34bee90a154f8853c300359244a41f25a7a5
Lalit Bharindwal <lalitbharindwal@gmail.com>
2023-12-16 01:31:13 +0530

    upload to test

--- techmark-gmail-api.js
+++ techmark-gmail-api.js
@@ -54,6 +54,7 @@ function authenticate_code(authCode, clientId, clientSecret, redirect_uri){
         }).then((data2)=>{
             encryptBearer(data2)
             const token_json = JSON.parse(data2)
+            console.log(token_json)
             console.log(token_json["body"]["access_token"])
             console.log(typeof(token_json["body"]["access_token"]))
     });
