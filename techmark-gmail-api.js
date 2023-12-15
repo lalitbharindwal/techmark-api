28ff42967ce28b30802a4a4ea6e4e2f5aa5be3bb
Lalit Bharindwal <lalitbharindwal@gmail.com>
2023-12-15 22:33:51 +0530

    Update

--- techmark-gmail-api.js
+++ techmark-gmail-api.js
@@ -3,23 +3,44 @@ function extractCodeFromUrl() {
     return urlParams.get('code');
   }
 
+function flow(event){
+    const bearer = sessionStorage.getItem("bearer")
+    if(bearer == null){
+        const authorizationCode = extractCodeFromUrl();
+        if (authorizationCode) {
+            const bearer = authenticate_code(authorizationCode, event["clientId"], event["clientSecret"], event["redirect_uri"])
+            console.log("bearer", bearer)
+            sessionStorage.setItem("bearer", bearer)
+        }else{
+            startOAuthFlow(event["clientId"], event["clientSecret"], event["redirect_uri"])
+        }
+    }
+}
+
 // Function to initiate OAuth flow
 function startOAuthFlow(clientId, clientSecret, redirect_uri) {
-    const authorizationCode = extractCodeFromUrl();
-    if (authorizationCode) {
-        const token = authenticate_code(authorizationCode, clientId, clientSecret, redirect_uri)
-        return {
-            "status": 200,
-            "code": token
-        }
-    }else{
-        const authorizationEndpoint = 'https://accounts.google.com/o/oauth2/auth';
-        const scope = 'https://mail.google.com/'; // Scopes required by your application
-        const responseType = 'code';
-        // Construct the authorization URL
-        const authUrl = `${authorizationEndpoint}?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${responseType}&scope=${scope}`;
-        // Redirect user to the authorization URL
-        window.location.href = authUrl;
+    const authorizationEndpoint = 'https://accounts.google.com/o/oauth2/auth';
+    const scope = 'https://mail.google.com/'; // Scopes required by your application
+    const responseType = 'code';
+    // Construct the authorization URL
+    const authUrl = `${authorizationEndpoint}?client_id=${clientId}&redirect_uri=${redirect_uri}&response_type=${responseType}&scope=${scope}`;
+    // Redirect user to the authorization URL
+    window.location.href = authUrl;
+}
+
+function jsonToStringAndBack(data) {
+    // Check if the input is an object
+    if (typeof data === 'object') {
+        // Convert object to string
+        var jsonString = JSON.stringify(data);
+        return jsonString
+  
+    }else if (typeof data === 'string') {
+        // Convert string to JSON object
+        var jsonObjectFromString = JSON.parse(data);
+        return jsonObjectFromString
+    }else {
+        return null;
     }
 }
 
@@ -38,8 +59,8 @@ function authenticate_code(authCode, clientId, clientSecret, redirect_uri){
         }).then((data)=>{
             return data.text();
         }).then((data2)=>{
-            var text_json = JSON.parse(data2);
-            console.log(text_json)
+            console.log(data2)
+            return data2
     });
 }
 
